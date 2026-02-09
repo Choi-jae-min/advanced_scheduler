2e4d04cba512489dc84400c903cbcc7d8e9c2550
jasmin <cjm98@smartel.co.kr>
2026-02-09 17:42:58 +0900

    Init : 스케쥴 관리 프로젝트 생성

---
+++ src/main/java/sparta/advancedscheduler/AdvancedSchedulerApplication.java
@@ -0,0 +1,13 @@
+package sparta.advancedscheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvancedSchedulerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdvancedSchedulerApplication.class, args);
+    }
+
+}
